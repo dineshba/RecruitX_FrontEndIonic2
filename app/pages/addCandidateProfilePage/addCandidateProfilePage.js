5d5e3bbb089323279d0d911c9fdcdaa832d305e4
dinesba <dineshudt17@gmail.com>
2016-01-07 17:04:33 +0530

    #2#2:D:Dinesh/Maha:Added few fields on create candidate screen

--- app/pages/addCandidateProfilePage/addCandidateProfilePage.js
+++ app/pages/addCandidateProfilePage/addCandidateProfilePage.js
@@ -1,15 +1,27 @@
 import {Page, NavController} from 'ionic/ionic';
 import {ScheduleInterviewPage} from '../scheduleInterviewPage/scheduleInterviewPage'
 
+class Candidate {
+  constructor(
+    public name: string,
+    public role: string
+  ){ }
+}
 @Page({
   templateUrl: 'build/pages/addCandidateProfilePage/addCandidateProfilePage.html',
 })
 export class AddCandidateProfilePage {
   constructor(nav:NavController) {
+    this.roles = ['Dev', 'QA', 'BA', 'PM', 'UI/UX'];
     this.nav = nav;
+    this.model =new Candidate();
   }
+//  model = new Candidate("Recruiter");
 
-  scheduleInterview() {
+ get diagnostic() {return JSON.stringify(this.model);}
+
+  scheduleInterview(args) {
+    if (args == true)
     this.nav.push(ScheduleInterviewPage);
   }
 }
