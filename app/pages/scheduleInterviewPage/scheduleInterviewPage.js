5dda4d964bfb9a6f642a4c688e6d8a4d01d2d8ba
Subha <subhamanoharanbe@gmail.com>
2016-01-07 16:58:28 +0530

    Kausalya/Subha: Adding date time picker

--- app/pages/scheduleInterviewPage/scheduleInterviewPage.js
+++ app/pages/scheduleInterviewPage/scheduleInterviewPage.js
@@ -7,5 +7,21 @@ import {Page} from 'ionic/ionic';
 export class ScheduleInterviewPage {
   constructor() {
 
+    var options = {
+    date: new Date(),
+    minDate: Date.now(),
+    allowOldDates: false,
+    mode: 'datetime'
+    };
+
+    function onSuccess(date) {
+    alert('Selected date: ' + date);
+    }
+
+    function onError(error) { // Android only
+    alert('Error: ' + error);
+    }
+
+    datePicker.show(options, onSuccess, onError);
   }
 }
